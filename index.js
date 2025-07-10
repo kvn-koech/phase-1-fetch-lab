e5a28ec8ad694bcf5c2847f5e5914b8f68fcc807
Kevin Koech <kevinkoech007@gmail.com>
2025-07-10 23:11:04 +0300

    complete test

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+     // Return the fetch request so tests can access it
+    return fetch('https://anapioficeandfire.com/api/books')
+      .then(response => response.json()) 
+      .then(booksData => {
+        renderBooks(booksData);  
+      })
+      .catch(error => console.error('Error fetching books:', error));
+  }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
